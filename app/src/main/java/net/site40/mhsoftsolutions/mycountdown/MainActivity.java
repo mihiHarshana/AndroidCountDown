20c05150e9f26861a83f390f5f488116e24f0ae6
Mihindu <mihinduwijesena@gmail.com>
2015-01-25 12:33:52 +0100

    Added close button and increased the ersion to V1.2

--- app/src/main/java/net/site40/mhsoftsolutions/mycountdown/MainActivity.java
+++ app/src/main/java/net/site40/mhsoftsolutions/mycountdown/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.Calendar;
@@ -80,7 +82,14 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
+
+    public void operClose(View view) {
+        Button btnClose = (Button) findViewById(R.id.btnClose);
+        finish();
+
+
+    }
+
 }
